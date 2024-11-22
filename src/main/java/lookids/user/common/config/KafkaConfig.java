72c1b73a7f51a619daa456fbf364c2a6c505b66a
PSangEon <qkr99102@naver.com>
2024-11-22 11:59:20 +0900

    Feat: 카프카 config 누락 추가
    KAN-273

--- src/main/java/lookids/user/common/config/KafkaConfig.java
+++ src/main/java/lookids/user/common/config/KafkaConfig.java
@@ -17,6 +17,7 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import lookids.user.userprofile.vo.in.CommentEventVo;
 import lookids.user.userprofile.vo.in.FeedEventVo;
+import lookids.user.userprofile.vo.in.ReplyEventVo;
 
 @EnableKafka
 @Configuration
@@ -43,6 +44,27 @@ public class KafkaConfig {
 	public ConcurrentKafkaListenerContainerFactory<String, CommentEventVo> commentEventListenerContainerFactory() {
 		ConcurrentKafkaListenerContainerFactory<String, CommentEventVo> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(commentConsumerFactory());
+		return factory;
+	}
+
+	@Bean
+	public ConsumerFactory<String, ReplyEventVo> replyConsumerFactory() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "comment-join-group");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+		//props.put(JsonDeserializer.TYPE_MAPPINGS, "lookids.commentread.comment.adaptor.in.kafka.vo.CommentEventVo");
+
+		return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(),
+			new ErrorHandlingDeserializer<>(new JsonDeserializer<>(ReplyEventVo.class, false)));
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, ReplyEventVo> replyEventListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, ReplyEventVo> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(replyConsumerFactory());
 		return factory;
 	}
 
